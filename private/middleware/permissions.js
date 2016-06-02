7e4d379f82a1edfb046260670b5166e377c2e094
ToPresa <topresa16@hotmail.com>
2016-06-02 15:50:51 +0100

    permissions

--- private/middleware/permissions.js
+++ private/middleware/permissions.js
@@ -10,10 +10,13 @@
 
 var url = req.url.split('/')[1];
         if (url == 'api') {
+console.log("url: "+req.url);
 
             // START REGION: API permissions (all)
             for (i = 0; i < routes.api.all.length; i++) {
+                console.log("url api: "+routes.api.all[i]);
                 if (routes.api.all[i].indexOf(req.url) > -1) {
+                    console.log("deu 1   "+routes.api.all[i]);
                     break;
                 }
             }
@@ -22,20 +25,59 @@ var url = req.url.split('/')[1];
             } else {
                 // END REGION: API permissions (all)
 
+ cookie.verifySession(req.cookies.session)
+                .catch(function (err) {
+                    console.log("caralho api:  "+err);
+                   return res.sendStatus(403);
+                });
                 // START REGION: API permissions (logged)
                cookie.verifySession(req.cookies.session)
-                    .then(function (userId) {
-                        for (i = 0; i < routes.api.logged.length; i++) {
-                            if (req.url.indexOf(routes.api.logged[i]) > -1) {
-                                break;
-                            }
-                        }
+                    .then(function (cenas) {
+                        var permission= cenas.permission;
+                        console.log("permission api: "+permission);
+                        switch(permission){
+                            case "0":
+                                for (i = 0; i < routes.api.logged.length; i++) {
+                                    if (req.url.indexOf(routes.api.logged[i]) > -1) {
+                                        break;
+                                    }
+                                }
+                                if (i == routes.api.logged.length) {
+                                    return res.sendStatus(403);
+                                } else {
+                                    next();
+                                }
+                            break;
+                            case "1":
+                                for (i = 0; i < routes.api.advanced.length; i++) {
+                                    if (req.url.indexOf(routes.api.advanced[i]) > -1) {
+                                        break;
+                                    }
+                                }
+                                if (i == routes.api.advanced.length) {
+                                    return res.sendStatus(403);
+                                } else {
+                                    next();
+                                }
+                            break;
+                            case "2": 
+                            console.log("entrou api");
+                                for (i = 0; i < routes.api.admin.length; i++) {
+                                    if (req.url.indexOf(routes.api.admin[i]) > -1) {
+                                        console.log("deu api");
+                                        break;
+                                    }
+                                }
+                                if (i == routes.api.admin.length) {
+                                    return res.sendStatus(403);
+                                } else {
+                                    next();
+                                }
+                            break;
+                        };
+                        
 
-                        if (i == routes.api.logged.length) {
-                            return res.sendStatus(403);
-                        } else {
-                            next();
-                        }
+                        
 
                     })
                     .catch(function (err) {
@@ -51,6 +93,7 @@ var url = req.url.split('/')[1];
             
             cookie.verifySession(req.cookies.session)
                 .catch(function (err) {
+                    console.log("caralho view:  "+err);
                     if (res.statusCode == null) {
                         res.redirect('/forbidden');
                     }
@@ -72,9 +115,11 @@ var url = req.url.split('/')[1];
                 // START REGION: Views permissions (logged)
 
                 cookie.verifySession(req.cookies.session)
-                    .then(function (permission) {
+                    .then(function (cenas) {
+                        var permission= cenas.permission;
+                        console.log("permission view: "+permission);
                         switch(permission){
-                            case 0:
+                            case "0":
                                 for (i = 0; i < routes.views.logged.length; i++) {
                                     if (url.indexOf(routes.views.logged[i]) > -1) {
                                         break;
@@ -89,7 +134,7 @@ var url = req.url.split('/')[1];
                                     next();
                                 }
                             break;
-                            case 1:
+                            case "1":
                                 for (i = 0; i < routes.views.advanced.length; i++) {
                                     if (url.indexOf(routes.views.advanced[i]) > -1) {
                                         break;
@@ -104,9 +149,13 @@ var url = req.url.split('/')[1];
                                     next();
                                 }
                             break;
-                            case 2:
+                            case "2":
+                            console.log("entrou  "+url);
                                 for (i = 0; i < routes.views.admin.length; i++) {
+                                    console.log("entrou com "+routes.views.admin[i]);
+                                    console.log("-----------------------");
                                     if (url.indexOf(routes.views.admin[i]) > -1) {
+                                        console.log("deu");
                                         break;
                                     }
                                 }
